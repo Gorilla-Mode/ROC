65af8e218dde5860ecd00d567960d7f9b289aae3
Gorilla-Mode <tobias_olsen02@hotmail.com>
2026-03-10 02:17:26 +0100

    feat: structs for orbits and bodies

--- main.c
+++ main.c
@@ -1,8 +1,56 @@
-﻿typedef struct celBody celBody;
+﻿#include <math.h>
+#include <stdint.h>
+typedef float f32_t;
+typedef double f64_t; //The fuck even is double, GIVE THE BITS
+typedef __float128 f128_t;
 
-struct celBody
+/**
+ * @brief Represents a celestial body
+ *
+ * @param GravSurf Surface gravity of the body (m/s^2)
+ * @param EqRadiusM Equatorial radius (m), approx distance from surface to center
+ * @param AtmHeightM Height above surface where atmosphere ends (m)
+ * @param GravParam Standard gravitational parameter (Mu) (m^3/s^2)
+ */
+typedef struct CelestBody CelestBody;
+
+/**
+ * @brief Represents an orbit
+ *
+ * @param PrimaryBody pointer to the body the orbit is around
+ * @param SMajorAxisM Semi-Major Axis (m), defines size of orbit
+ * @param Eccentricity Defines shape of orbit (0 <= e < 1)
+ */
+typedef struct Orbit Orbit;
+
+struct CelestBody
 {
-    int x;
-    int y;
+    f64_t GravSurf;
+    f64_t GravParam;
+    uint64_t EqRadiusM;
+    int32_t AtmHeightM;
 };
 
+struct Orbit
+{
+    CelestBody *PrimaryBody;
+
+    f64_t SMajorAxisM;
+    f64_t Eccentricity;
+
+    f64_t (*OPeriod)       (const Orbit *orbit);
+    f64_t (*OAltitude)     (const Orbit *orbit);
+    f64_t (*Periapsis)     (const Orbit *orbit);
+    f64_t (*Apoapsis)      (const Orbit *orbit);
+    Orbit (*ResonantOrbit) (const Orbit *orbit, uint32_t satelliteCount, f64_t desiredAltitude);
+};
+
+f64_t CalcOrbitalPeriod(const Orbit *orbit)
+{
+    return 2.0 * M_PI * sqrt(pow(orbit->SMajorAxisM, 3) / orbit->PrimaryBody->GravParam);
+}
+
+f64_t DeltaV(const Orbit *orbitBase, const Orbit *orbitTarget)
+{
+    return -1;
+}
